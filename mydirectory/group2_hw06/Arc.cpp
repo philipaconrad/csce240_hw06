5715433b2455a55cd6daa2014e69f4c18bed6caa
David Warren <warrenwd@email.sc.edu>
2014-04-03 22:52:17 -0400

    Code Cleanup
    Went through and indented with 4-space tabs.

--- mydirectory/group2_hw06/Arc.cpp
+++ mydirectory/group2_hw06/Arc.cpp
@@ -4,54 +4,56 @@ static const string TAG = "ZORK: ";
 
 Arc::Arc()
 {
-this->x = DUMMYX;
-this->y = DUMMYY;
+    this->x = DUMMYX;
+    this->y = DUMMYY;
 }
 
 Arc::~Arc()
 {
+    
 }
 
 int Arc::getX()
 {
-return this->x;
+    return this->x;
 }
 
 int Arc::getY()
 {
-return this->y;
+    return this->y;
 }
 
 void Arc::setX(int value)
 {
-this->x = value;
+    this->x = value;
 }
 
 void Arc::setY(int value)
 {
-this->y = value;
+    this->y = value;
 }
 
 bool Arc::equals(Arc that)
 {
-if((this->x == that.getX()) &&
-(this->y == that.getY()))
-{
-return true;
-}
+    if((this->x == that.getX()) && (this->y == that.getY()))
+    {
+        return true;
+    }
 
-return false;
+    return false;
 }
 
 bool Arc::z()
 {
-return (this->x = this->y);
+    return (this->x = this->y);
 }
 
 string Arc::toString()
 {
-string s = "";
-s += "(" + Utils::Format(this->getX(), 3) + " -> ";
-s += Utils::Format(this->getY(), 3) + ")";
-return s;
+    string s = "";
+    
+    s += "(" + Utils::Format(this->getX(), 3) + " -> ";
+    s += Utils::Format(this->getY(), 3) + ")";
+    
+    return s;
 }
